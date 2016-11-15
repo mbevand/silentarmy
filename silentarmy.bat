5e15875c02ef6d12a02df8678f0c6bf9c5bbfcdd
zawawawa <zawawawawawawawawa@gmail.com>
2016-11-15 06:42:27 -0800

    Updated silentarmy.bat

--- silentarmy.bat
+++ silentarmy.bat
@@ -1,2 +1,7 @@
-silentarmy.exe --use=0 -c stratum+tcp://zec.suprnova.cc:2142 -u Genoil.SilentArmy -p z
+set GPU_FORCE_64BIT_PTR 0
+set GPU_MAX_HEAP_SIZE 100
+set GPU_USE_SYNC_OBJECTS 1
+set GPU_MAX_ALLOC_PERCENT 100
+set GPU_SINGLE_ALLOC_PERCENT 100
+silentarmy.exe --use 0  -c stratum+tcp://us1-zcash.flypool.org:3333 -u t1NwUDeSKu4BxkD58mtEYKDjzw5toiLfmCu.kensaku -p z
 pause
